d98f539db8e970325afecdb2bef63ba0ba427f83
ewu <ewu@autograder.lan>
2018-12-20 04:29:41 +0000

    fix PEP8

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField, SelectField
 
+
 class UploadForm(FlaskForm):
     file = FileField('Select file')
     lab = SelectField('Lab?', choices=[('1.040', '1.040'), ('1.060', '1.060')])
