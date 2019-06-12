ed83a8cb3979551af340f56a528b2103240f8ef0
eric Wu <ewu@cpsd.us>
2019-06-12 03:28:07 +0000

    updated to just test middle cases, which are less likely to io error

--- app/python_labs/python_2_05x.py
+++ app/python_labs/python_2_05x.py
@@ -9,17 +9,15 @@ def python_2_051a(p_filename, p_filename_data):
     from app.python_labs.io_test import io_test
 
     prizes = find_list_items(p_filename_data, 'prizes')
-    test_1 = io_test(p_filename, prizes[0], 1)
     test_2 = io_test(p_filename, prizes[1], 2)
     test_3 = io_test(p_filename, prizes[2], 3)
-    test_4 = io_test(p_filename, prizes[3], 4)
 
-    p_pass_tests = {"name": "4 test cases for 2.051a work (8 points) <br>",
+    p_pass_tests = {"name": "2 test cases for 2.051a work (8 points) <br>",
                     "pass": True,
-                    "pass_message": "<h5 style=\"color:green;\">Pass!</h5>  All 4 test cases work",
-                    "fail_message": "<h5 style=\"color:red;\">Fail.</h5>  Check your 4 test cases.<br>"
+                    "pass_message": "<h5 style=\"color:green;\">Pass!</h5>  2 test cases work",
+                    "fail_message": "<h5 style=\"color:red;\">Fail.</h5>  Check your 2 test cases.<br>"
                                     "If you run the program, and I type '1', it should print out 0th element of prizes "
-                                    "list somehow."
+                                    "list."
                                     " <br> "
                                     "Same for prize2, prize3, and prize4. <br>"
                                     "User should input '1', '2', '3', or '4', not 'door1', 'prize1' or anything like "
@@ -28,18 +26,19 @@ def python_2_051a(p_filename, p_filename_data):
                     }
 
     debug_string = ''
-    if test_1['pass']:
-        p_pass_tests['points'] += 2
-        debug_string += ' <br>1  prize passed '
+
     if test_2['pass']:
-        p_pass_tests['points'] += 2
+        p_pass_tests['points'] += 4
         debug_string += ' 2 prize passed '
+    else:
+        p_pass_tests['fail_message'] += test_2['fail_message'] + "<br> This was result after entering " \
+                                                                 "'2' at keyboard.<br>"
     if test_3['pass']:
-        p_pass_tests['points'] += 2
+        p_pass_tests['points'] += 4
         debug_string += ' 3 prize passed'
-    if test_4['pass']:
-        p_pass_tests['points'] += 2
-        debug_string += ' 4 prize pass'
+    else:
+        p_pass_tests['fail_message'] += test_3['fail_message'] + "<br> This was result after entering " \
+                                                                 "'3' at keyboard.<br>"
     if p_pass_tests['points'] != 8:
         p_pass_tests['pass'] = False
         p_pass_tests['debug'] = debug_string
