d436cdf1d0bb7a82d89fcef58e833375958f4e79
ewu <ewu@ubuntu.cpsd.us>
2019-06-14 20:13:14 +0000

    fixed bug where extract_function fails if there is no main code

--- app/python_labs/function_test.py
+++ app/python_labs/function_test.py
@@ -9,12 +9,15 @@ def run_unit_test(p_lab, p_test_number, p_points):
     import delegator
     import re
 
-    p_test_function = {"name": "Testing calling functions, test " + str(p_test_number) + ".  (" + str(p_points) +
-                               " points )",
-                       "pass": True,
-                       "pass_message": "Pass! Test number " + str(p_test_number) + " gave expected result. <br>",
-                       "fail_message": "Fail. Test number " + str(p_test_number) + " gave unexpected result. <br>",
-                       }
+    p_unit_test = {"name": "Testing calling functions, test " + str(p_test_number) + ".  (" + str(p_points) +
+                           " points )",
+                   "pass": True,
+                   "pass_message": "<h5 style=\"color:green;\">Pass!</h5> Test number " + str(p_test_number) +
+                                   " gave expected result. <br>",
+                   "fail_message": "<h5 style=\"color:red;\">Fail.</h5> Test number " + str(p_test_number) +
+                                   " gave unexpected result. <br>",
+                   'points': 0
+                   }
 
     cmd = 'python3 /tmp/' + str(p_lab) + '.test.py testAutograde.test_' + str(p_test_number) + " 2>&1 "
     c = delegator.run(cmd)
@@ -22,18 +25,20 @@ def run_unit_test(p_lab, p_test_number, p_points):
     print(cmd)
     error = re.search('Error', c.out, re.X | re.M | re.S)
     failed_assertion = re.search('AssertionError', c.out, re.X | re.M | re.S)
-    OK = re.search('^OK$', c.out, re.X | re.M | re.S)
+    ok = re.search(r'^OK$', c.out, re.X | re.M | re.S)
 
     formatted_cout = re.sub(r"\n", "<br>", c.out)
     if failed_assertion:
-        p_test_function['pass'] = False
-        p_test_function['fail_message'] += "Function ran but gave the wrong result. <br>"
-        p_test_function['fail_message'] += formatted_cout + "<br>"
+        p_unit_test['pass'] = False
+        p_unit_test['fail_message'] += "Function ran but gave the wrong result. <br>"
+        p_unit_test['fail_message'] += formatted_cout + "<br>"
     elif error:
-        p_test_function['pass'] = False
-        p_test_function['fail_message'] += "Function didn't run at all.  Check for coding errors. <br>"
-        p_test_function['fail_message'] += formatted_cout + "<br>"
-    return p_test_function
+        p_unit_test['pass'] = False
+        p_unit_test['fail_message'] += "Function didn't run at all.  Check for coding errors. <br>"
+        p_unit_test['fail_message'] += formatted_cout + "<br>"
+    else:
+        p_unit_test['points'] += p_points
+    return p_unit_test
 
 
 # Inputs: p_filename, filename with python in it
@@ -72,22 +77,48 @@ def create_testing_file(p_filename):
 # Output: none
 # This module extracts all functions from a python file and puts them in a file outputfile.functions.py
 def extract_all_functions(orig_file):
+    """
+    Extracts all functions
+    :param orig_file: The name of the python file (string)
+    :return: Nothing. It writes everything to the file <python file>.functions.py
+    """
     import re
     outfile_name = orig_file.replace('.py', '.functions.py')
     outfile = open(outfile_name, 'w')
+    print("starting now! " + str(outfile_name))
+    print("starting now! " + str(orig_file))
+
     with open(orig_file, 'r', encoding='utf8') as infile:
         line = True
+
         while line:
+            print("aaa " + str(line))
             line = infile.readline()
-            start_def = re.search("^(def|class) \s+ .+ " , line,  re.X | re.M | re.S)
+            print("aaa " + str(line))
+
+            start_def = re.search(r"^(def|class) \s+ .+ ", line,  re.X | re.M | re.S)
+            print(start_def)
+
+            empty_counter = 0
             if start_def:
+
+                print("aaa " + str(line))
                 outfile.write(line)
                 in_function = True
+                # counter = 0
                 while in_function:
+                    # counter += 1
+                    # print("counter " + str(counter))
+                    # if counter == 15:
+                    #     raise Exception("what the heeck")
                     line = infile.readline()
-                    end_of_function = re.search("^[a-zA-Z]", line, re.X | re.M | re.S)
-                    new_function = re.search("^(def|class) \s+ .+ " , line,  re.X | re.M | re.S)
-
+                    if line == '':
+                        break
+                    # print("aaa next line " + line)
+                    end_of_function = re.search(r"^[a-zA-Z]", line, re.X | re.M | re.S)
+                    new_function = re.search(r"^(def|class) \s+ .+ ", line,  re.X | re.M | re.S)
+                    # print("end? " + str(end_of_function))
+                    # print("new?" + str(new_function))
                     if end_of_function and not new_function:
                         in_function = False
                         start_def = False
@@ -95,9 +126,11 @@ def extract_all_functions(orig_file):
                         in_function = True
                         start_def = True
                         outfile.write(line)
-
                     else:
                         outfile.write(line)
+                    # print("aaa this line still " + str(line))
+                    # print("in fucntion start_def " + str(in_function) + " " + str(start_def))
+
 
 
 def extract_single_function(p_orig_file, p_function):
