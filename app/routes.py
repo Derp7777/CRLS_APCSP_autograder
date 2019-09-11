c713343cb5433eb09b0efffbfe38d9ec84fde648
Eric Wu <dimmyfinster@samanthsmacbook.cpsd.us>
2019-09-11 12:31:08 -0400

    update for bugs

--- app/routes.py
+++ app/routes.py
@@ -761,6 +761,55 @@ def docs_feedback_hardware_memory():
     return render_template('feedback.html', user=user, tests=tests, filename=link, score_info=score_info)
 
 
+@app.route('/docs/hardware_storage')
+def docs_feedback_hardware_storage():
+    from app.docs_labs.docs import get_text, exact_answer, keyword_and_length
+
+    user = {'username': 'CRLS Scratch Scholar'}
+    tests = list()
+    score_info = {'score': 0, 'max_score': 22, 'manually_scored': 78, 'finished_scoring': True}
+
+    link = request.args['link']
+    text = get_text(link)
+
+    print(text)
+    test1c = exact_answer('1c. How much memory?', [r'1c\. .+? tabledata \s* (8|16|32) .*? g .+? 1d\.'], text, points=5)
+    test1d = exact_answer('1d. ECC?', [r'1d\. .+? tabledata \s* n .+? 1e\.'], text, points=5)
+    test1e = exact_answer('1e. Full memory test. How long??', [r'1d\. .+? tabledata \s* [0-9] .+? 1f\.'],
+                          text, points=1)
+    test1f = exact_answer('1f. Memory speed and type', [r'1f\. .+? tabledata .+? [0-9] .+? 1g\.',
+                                                        r'1f\. .+? tabledata .+? ddr .+? 1g\.'],
+                          text, required=2, points=1)
+    test1g = exact_answer('1g. Full memory test 192G. How long??', [r'1g\. .+? tabledata \s* [0-9] .+? memory'],
+                          text, points=1)
+    test2b = exact_answer('2b. removed. Teacher marks?', [r'2b\. .+? tabledata .+? [a-z] .+? 2c\.'], text, points=1)
+    test2c = exact_answer('2c. replaced. Teacher marks?', [r'2c\. .+? tabledata .+? [a-z] .+? how'], text, points=1)
+    test3a = exact_answer('3a. screenshot memory', [r'3a\. .+? tabledata \s aaa \s inlineobject .+? picking'],
+                          text, points=1)
+    test4a = exact_answer('4a. Memory speed and type', [r'4a\. .+? tabledata .+? [0-9] .+? 4b\.',
+                                                        r'4a\. .+? tabledata .+? ddr .+? 4b\.',
+                                                        r'4a\. .+? tabledata .+? ecc .+? 4b\.'],
+                          text, required=3, points=1)
+    test4b = exact_answer('4b. Max memory desktop?', [r'4b\. .+? tabledata \s* [0-9] .*? g .+? pick'], text, points=1)
+    test5a = exact_answer('5a. Memory speed and type', [r'5a\. .+? tabledata .+? [0-9] .+? 5b\.',
+                                                        r'5a\. .+? tabledata .+? ddr .+? 5b\.',
+                                                        r'5a\. .+? tabledata .+? ecc .+? 5b\.'],
+                          text, required=3, points=1)
+    test5b = exact_answer('5b. Max memory server?', [r'5b\. .+? tabledata \s* [0-9] .*? g .+? comparison'],
+                          text, points=1)
+
+    test6a = keyword_and_length('6a. Compare server/desktop memory', [r'[a-zA-Z]+'], text,
+                                search_string=r'6a\. .+? tabledata (.+) laptop', min_length=15, points=1)
+    test7a = keyword_and_length('7a. sodimm', [r'[a-zA-Z]+'], text,
+                                search_string=r'7a\. .+? tabledata (.+) check', min_length=7, points=1)
+    tests.extend([test1c, test1d, test1e, test1f, test1g, test2b, test2c, test3a, test4a, test4b, test5a, test5b,
+                  test6a, test7a])
+    for test in tests:
+        if test['pass']:
+            score_info['score'] += test['points']
+    return render_template('feedback.html', user=user, tests=tests, filename=link, score_info=score_info)
+
+
 @app.route('/docs/hexadecimal_numbers_v3')
 def docs_feedback_hexadecimal_numbers_v3():
     from app.docs_labs.docs import get_text, exact_answer, keyword_and_length
@@ -1347,14 +1396,22 @@ def docs_feedback_python_1030():
                                 search_string=r'\s6b\. .+? tabledata (.+) 7a\.', min_length=5, points=1)
     test7b = keyword_and_length('question 7b', [r'[a-zA-Z]+'], text,
                                 search_string=r'\s7b\. .+? tabledata (.+) 8\.', min_length=5, points=1)
-    test8a = keyword_and_length('question 8a', [r'number \s* = \s* 100'], text,
-                                search_string=r'\s8\. .+? tabledata (.+) create\sa\svariable', min_length=5, points=2.5)
-    test8b = keyword_and_length('question 8b', [r'print \s* \(number\)'], text,
-                                search_string=r'\s8\. .+? tabledata (.+) create\sa\svariable', min_length=5, points=2.5)
-    test8c = keyword_and_length('question 8c', [r'number2 .+? 100'], text,
-                                search_string=r'\s8\. .+? tabledata (.+) create\sa\svariable', min_length=5, points=2.5)
-    test8d = keyword_and_length('question 8d', [r'print \s* \(number2\)'], text,
-                                search_string=r'\s8\. .+? tabledata (.+) create\sa\svariable', min_length=5, points=2.5)
+    test8a = exact_answer('question 8a', [r'8\. .+? tabledata .+? number \s* = \s* 100 .+? Create\sa\svariable'],
+                          text, points=2.5)
+    test8b = exact_answer('question 8b', [r'8\. .+? tabledata .+? print \s* \(number\) .+? Create\sa\svariable'],
+                          text, points=2.5)
+    test8c = exact_answer('question 8c', [r'8\. .+? tabledata .+? number2 .+? 100 .+? Create\sa\svariable'],
+                          text, points=2.5)
+    test8d = exact_answer('question 8d', [r'8\. .+? tabledata .+? print \s* \(number2\) .+? Create\sa\svariable'],
+                          text, points=2.5)
+    # test8a = keyword_and_length('question 8a', [r'number \s* = \s* 100'], text,
+    #                             search_string=r'\s8\. .+? tabledata (.+?) Create\sa\svariable', min_length=5, points=2.5)
+    # test8b = keyword_and_length('question 8b', [r'print \s* \(number\)'], text,
+    #                             search_string=r'\s8\. .+? tabledata (.+?) Create\sa\svariable', min_length=5, points=2.5)
+    # test8c = keyword_and_length('question 8c', [r'number2 .+? 100'], text,
+    #                             search_string=r'\s8\. .+? tabledata (.+?) Create\sa\svariable', min_length=5, points=2.5)
+    # test8d = keyword_and_length('question 8d', [r'print \s* \(number2\)'], text,
+    #                             search_string=r'\s8\. .+? tabledata (.+?) Create\sa\svariable', min_length=5, points=2.5)
 
 
     tests.extend([test1a, test1b, test1c, test2a, test2b, test2c, test3a, test3b, test3c, test4a, test4b, test4c,
